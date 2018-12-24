c7efafabc57c2e58134971b480d77370c2c6b6f7
Kremery <kremery@ukr.net>
2018-12-25 00:02:10 +0200

    I'm working on decoding text

--- weatherapp.py
+++ weatherapp.py
@@ -11,12 +11,12 @@ ACCU_URL = "https://www.accuweather.com/uk/ua/kaniv/321864/weather-forecast/3218
 
 #N2
 # getting page from server
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
 accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
 accu_page = str(accu_page)
 
-
+#here is the temperature
 ACCU_TEPM_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEPM_TAG)
 accu_tepm_tag_index = accu_page.find(ACCU_TEPM_TAG)
@@ -28,9 +28,31 @@ for char in accu_page[accu_temp_value_start:]:
 	else:
 		break
 
-#print(accu_temp_value_start)
-#print(accu_temp)
-print('AccuWeather: \n')
-print(f'Temperature: {html.unescape(accu_temp)}\n')
 
-#print(accu_page) 
+
+#here is the weather
+ACCU_WEATHER_TAG = '<span class="cond">'
+accu_weather_tag_size = len(ACCU_WEATHER_TAG)
+accu_weather_tag_index = accu_page.find(ACCU_WEATHER_TAG)
+accu_weather_value_start = accu_weather_tag_index + accu_weather_tag_size
+accu_weather = ''
+
+
+
+for charW in accu_page[accu_weather_value_start:]:
+	if charW != '<':
+		accu_weather += charW
+	else:
+		break
+		
+		
+
+print("Start: ", accu_weather_value_start)
+print("Index: ", accu_weather_tag_index)
+print("accu_weather = ", accu_weather)
+print(accu_weather_tag_size)
+print('AccuWeather in Kaniv: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
+print(f'Weather: {html.unescape(accu_weather)}\n')
+
+#print("PAGE: ", accu_page) 
