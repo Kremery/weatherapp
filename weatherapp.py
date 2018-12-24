be2dfcd7bf57d038dc8385a1090b401433112a63
Kremery <kremery@ukr.net>
2018-12-24 17:49:20 +0200

    show temerature in Kaniv

--- weatherapp.py
+++ weatherapp.py
@@ -1,8 +1,8 @@
 """
-Weather app my project.
+Weatherapp my project.
 """
 
-
+import html
 from urllib.request import urlopen, Request
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/kaniv/321864/weather-forecast/321864"
@@ -10,8 +10,27 @@ ACCU_URL = "https://www.accuweather.com/uk/ua/kaniv/321864/weather-forecast/3218
 #response = urlopen(ACCU_URL)
 
 #N2
+# getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64;)'}
 accu_request = Request(ACCU_URL, headers=headers)
-response = urlopen(accu_request)
+accu_page = urlopen(accu_request).read()
+accu_page = str(accu_page)
 
-print(response)
+
+ACCU_TEPM_TAG = '<span class="large-temp">'
+accu_temp_tag_size = len(ACCU_TEPM_TAG)
+accu_tepm_tag_index = accu_page.find(ACCU_TEPM_TAG)
+accu_temp_value_start = accu_tepm_tag_index + accu_temp_tag_size
+accu_temp = ''
+for char in accu_page[accu_temp_value_start:]:
+	if char != '<':
+		accu_temp += char
+	else:
+		break
+
+#print(accu_temp_value_start)
+#print(accu_temp)
+print('AccuWeather: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
+
+#print(accu_page) 
