b8f7e8e049f1ade1efb9e7841d08501a8554dcd5
Kremery <kremery@ukr.net>
2019-01-04 17:40:29 +0200

    Added temperature index to RP5 site

--- weatherapp.py
+++ weatherapp.py
@@ -30,7 +30,7 @@ for char in accu_page[accu_temp_value_start:]:
 		break
 
 
-#тут виводимо данястану погоди
+#тут виводимо дані стану погоди
 #here is the weather
 ACCU_COND_TAG = '<span class="cond">'
 accu_cond_tag_size = len(ACCU_COND_TAG)
@@ -53,3 +53,47 @@ print(f'Temperature: {html.unescape(accu_temp)}\n')
 print(f'Weather: {accu_cond}\n')
 
 #print("PAGE: ", accu_page) 
+
+RP5_URL = "http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9A%D0%B0%D0%BD%D0%B5%D0%B2%D1%96"
+
+#headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
+rp5_request = Request(RP5_URL, headers=headers)
+rp5_page = urlopen(rp5_request).read()
+
+rp5_content = rp5_page.decode('utf-8')
+#here is the temperature
+
+WINFO_CONTAINER_TAG = '<div id="ArchTemp">'
+RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
+rp5_temp_tag = rp5_content.find(RP5_TEMP_TAG, rp5_content.find(WINFO_CONTAINER_TAG))
+rp5_temp_tag_size = len(RP5_TEMP_TAG)
+#rp5_tepm_tag_index = rp5_content.find(rp5_temp_tag)
+rp5_temp_tag_start = rp5_temp_tag + rp5_temp_tag_size
+rp5_temp = ''
+for char in rp5_content[rp5_temp_tag_start:]:
+	if char != '<':
+		rp5_temp += char
+	else:
+		break
+"""
+#тут виводимо дані стану погоди
+#here is the weather
+ACCU_COND_TAG = '<span class="cond">'
+accu_cond_tag_size = len(ACCU_COND_TAG)
+accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
+accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
+accu_cond = ''
+
+
+
+for char in accu_page[accu_cond_value_start:]:
+	if char != '<':
+		accu_cond += char
+	else:
+		break
+"""		
+
+
+print('RP5 in Kaniv: \n')
+print(f'Temperature: {html.unescape(rp5_temp)}\n')
+#print(f'Weather: {accu_cond}\n')
