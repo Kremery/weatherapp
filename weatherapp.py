b801b16985aecbe70c7f5852f18602c4bf272bf2
Kremery <kremery@ukr.net>
2019-01-15 20:25:07 +0200

    changed the tag for the RP5 site

--- weatherapp.py
+++ weatherapp.py
@@ -12,7 +12,7 @@ RP5_URL = "http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9A%D0%B
 #RP5_TAGS = (rp5_content.find(RP5_TEMP_TAG, rp5_content.find(WINFO_CONTAINER_TAG)), ',"°F</span>')
 #WINFO_CONTAINER_TAG = '<div id="ArchTemp">'
 #RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
-RP5_TAGS = ('<span class="t_0" style="display: block;">', ',"°F</span>')
+RP5_TAGS = ('<span class="t_0" style="display: block;">', '<span class="t_0" style="display: block;">')
 
 
 
@@ -37,7 +37,7 @@ def produce_output(provider_name, temp, condition):
 
 	print(f'\n {provider_name}')
 	print(f'Temperature: {html.unescape(temp)}\n')
-	print(f'Condition: {condition}\n')
+	print(f'Condition: {html.unescape(condition)}\n')
 	
 
 def get_tag_content(page_content, tag):
@@ -84,13 +84,11 @@ if __name__ == '__main__':
 '''
 #N1
 #response = urlopen(ACCU_URL)
-
 #N2
 # getting page from server
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
 accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
-
 #accu_page = str(accu_page)
 accu_page = accu_page.decode('utf-8')
 #here is the temperature
@@ -104,8 +102,6 @@ for char in accu_page[accu_temp_value_start:]:
 		accu_temp += char
 	else:
 		break
-
-
 #тут виводимо дані стану погоди
 #here is the weather
 ACCU_COND_TAG = '<span class="cond">'
@@ -113,34 +109,23 @@ accu_cond_tag_size = len(ACCU_COND_TAG)
 accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
 accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
 accu_cond = ''
-
-
-
 for char in accu_page[accu_cond_value_start:]:
 	if char != '<':
 		accu_cond += char
 	else:
 		break
 		
-
-
 print('AccuWeather in Kaniv: \n')
 print(f'Temperature: {html.unescape(accu_temp)}\n')
 print(f'Weather: {accu_cond}\n')
-
 #print("PAGE: ", accu_page) 
-
-
 #this site rp5.ua
 RP5_URL = "http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9A%D0%B0%D0%BD%D0%B5%D0%B2%D1%96"
-
 #headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
 rp5_request = Request(RP5_URL, headers=headers)
 rp5_page = urlopen(rp5_request).read()
-
 rp5_content = rp5_page.decode('utf-8')
 #here is the temperature
-
 WINFO_CONTAINER_TAG = '<div id="ArchTemp">'
 RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
 rp5_temp_tag = rp5_content.find(RP5_TEMP_TAG, rp5_content.find(WINFO_CONTAINER_TAG))
@@ -153,7 +138,6 @@ for char in rp5_content[rp5_temp_tag_start:]:
 		rp5_temp += char
 	else:
 		break
-
 #тут виводимо дані стану погоди
 #here is the weather
 AINFO_CONTAINER_TAG = '<div class="ArchiveInfo">'
@@ -170,23 +154,16 @@ for char in rp5_content[rp5_archive_tag_start:]:
 		rp5_archive = rp5_archive + '.'
 		break
 		
-
-
 print('RP5 in Kaniv: \n')
 print(f'Temperature: {html.unescape(rp5_temp)}\n')
 print(f'Weather: {rp5_archive}\n')
-
-
 #this site sinoptik.ua
 SINOPTIK_URL = "https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B0%D0%BD%D1%96%D0%B2/10-%D0%B4%D0%BD%D1%96%D0%B2"
-
 #headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
 sinoptik_request = Request(SINOPTIK_URL, headers=headers)
 sinoptik_page = urlopen(sinoptik_request).read()
-
 sinoptik_content = sinoptik_page.decode('utf-8')
 #here is the temperature
-
 SINFO_CONTAINER_TAG = '<div class="lSide">'
 SINOPTIK_TEMP_TAG = '<p class="today-temp">'
 sinoptik_temp_tag = sinoptik_content.find(SINOPTIK_TEMP_TAG, sinoptik_content.find(SINFO_CONTAINER_TAG))
@@ -199,9 +176,6 @@ for char in sinoptik_content[sinoptik_temp_tag_start:]:
 		sinoptik_temp += char
 	else:
 		break
-
-
-
 #тут виводимо дані стану погоди
 #here is the weather
 SSINFO_CONTAINER_TAG = '<div class="wDescription clearfix">'
@@ -217,8 +191,6 @@ for char in sinoptik_content[sinoptik_archive_tag_start:]:
 	else:
 		break
 		
-
-
 print('SINOPTIK.UA in Kaniv: \n')
 print(f'Temperature: {html.unescape(sinoptik_temp)}\n')
 print(f'Weather: {sinoptik_archive}\n')
