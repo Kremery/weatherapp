ff8250cd5b86bbd6adcc16b3ab58955ea3e8837a
Kremery <kremery@ukr.net>
2019-02-03 13:30:27 +0200

    change the function get_weather_info to get information throudh the library BeautifulSoup

--- weatherapp.py
+++ weatherapp.py
@@ -4,6 +4,7 @@ Weatherapp my project.
 import sys
 import html
 import argparse
+from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/kaniv/321864/weather-forecast/321864"
@@ -33,16 +34,6 @@ def get_page_source(url):
 	request = Request(url, headers=get_request_headers())
 	page_sourse = urlopen(request).read()
 	return page_sourse.decode('utf-8')
-
-
-def produce_output(provider_name, temp, condition):
-	"""функція що виводить інформацю із сайтів
-	   function that displays information from sites
-	"""
-
-	print(f'\n {provider_name}')
-	print(f'Temperature: {html.unescape(temp)}\n')
-	print(f'Condition: {html.unescape(condition)}\n')
 	
 
 def get_tag_content(page_content, tag):
@@ -62,11 +53,58 @@ def get_tag_content(page_content, tag):
 	return content
 
 
-def get_weather_info(page_content, tags):
+def get_weather_info(page_content):
 	"""
 	"""
-	return tuple([get_tag_content(page_content, tag) for tag in tags])
+	city_page = BeautifulSoup(page_content, 'html.parser')
+	current_day_section = city_page.find(
+		'li', class_='day current first cl')
 
+	weather_info = {}
+	if current_day_section:
+		current_day_url = current_day_section.find('a').attrs['href']
+		if current_day_url:
+			current_day_page = get_page_source(current_day_url)
+			if current_day_page:
+				current_day = \
+					BeautifulSoup(current_day_page, 'html.parser')
+				weather_details = \
+					current_day.find('div', attrs={'id': 'detail-now'})
+				
+				condition = weather_details.find('span', class_='cond')
+				if condition:
+					weather_info['cond'] = condition.text
+				
+				temp = weather_details.find('span', class_='large-temp')
+				if temp:
+					weather_info['temp'] = temp.text
+				
+				feal_temp = weather_details.find('span', class_='small-temp')
+				if feal_temp:
+					weather_info['feal_temp'] = feal_temp.text
+				
+				wind_info = weather_details.find_all('li', class_='wind')
+				if wind_info:
+					weather_info['wind'] = \
+					' '.join(map(lambda t: t.text.strip(), wind_info))
+
+	return weather_info
+
+
+def produce_output(info):
+	"""функція що виводить інформацю із сайтів
+	   function that displays information from sites
+	"""
+
+	print('Accu Weather: \n')
+
+	for key, value in info.items():
+		print(f'{key}: {html.unescape(value)}')
+
+	"""print(f'\n {provider_name}')
+	print(f'Temperature: {html.unescape(temp)}\n')
+	print(f'Condition: {html.unescape(condition)}\n')
+	"""
 
 def main(argv):
 	""" Main entry point.
@@ -96,8 +134,7 @@ def main(argv):
 	for name in weather_sites:
 		url, tags = weather_sites[name]
 		content = get_page_source(url)
-		temp, condition = get_weather_info(content, tags)
-		produce_output(name, temp, condition)
+		produce_output(get_weather_info(content))
 
 
 if __name__ == '__main__':
