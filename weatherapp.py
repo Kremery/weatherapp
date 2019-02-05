be8313fabca4508bd1204c167cbd8b8fedc5f18e
Kremery <kremery@ukr.net>
2019-02-05 19:21:42 +0200

    added the ability to choose a block day or night

--- weatherapp.py
+++ weatherapp.py
@@ -57,10 +57,12 @@ def get_weather_info(page_content):
 	"""
 	"""
 	city_page = BeautifulSoup(page_content, 'html.parser')
+	
 	current_day_section = city_page.find(
 		'li', class_='day current first cl')
 
 	weather_info = {}
+
 	if current_day_section:
 		current_day_url = current_day_section.find('a').attrs['href']
 		if current_day_url:
@@ -91,7 +93,43 @@ def get_weather_info(page_content):
 				stats_info = weather_details.find_all('ul', class_='stats')
 				if stats_info:
 					weather_info['stats'] = \
-					' '.join(map(lambda t: t.text.strip(), stats_info))	
+					' '.join(map(lambda t: t.text.strip(), stats_info))
+
+	else:
+		current_day_section = city_page.find(
+		'li', class_='night current first cl')
+
+		if current_day_section:
+			current_day_url = current_day_section.find('a').attrs['href']
+			if current_day_url:
+				current_day_page = get_page_source(current_day_url)
+				if current_day_page:
+					current_day = \
+						BeautifulSoup(current_day_page, 'html.parser')
+					weather_details = \
+						current_day.find('div', attrs={'id': 'detail-now'})
+					
+					condition = weather_details.find('span', class_='cond')
+					if condition:
+						weather_info['cond'] = condition.text
+					
+					temp = weather_details.find('span', class_='large-temp')
+					if temp:
+						weather_info['temp'] = temp.text
+					
+					feal_temp = weather_details.find('span', class_='small-temp')
+					if feal_temp:
+						weather_info['feal_temp'] = feal_temp.text
+				
+					'''wind_info = weather_details.find_all('li', class_='wind')
+					if wind_info:
+						weather_info['wind'] = \
+						' '.join(map(lambda t: t.text.strip(), wind_info))'''
+
+					stats_info = weather_details.find_all('ul', class_='stats')
+					if stats_info:
+						weather_info['stats'] = \
+						' '.join(map(lambda t: t.text.strip(), stats_info))
 
 	return weather_info
 
