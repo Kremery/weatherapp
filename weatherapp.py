92e4ac93699a3a7980f9e3633cfdb387e9196729
Kremery <kremery@ukr.net>
2019-02-23 08:06:10 +0200

    created 2 new function get_locations, configurate' q exit

--- weatherapp.py
+++ weatherapp.py
@@ -1,19 +1,23 @@
 """
 Weatherapp my project.
+Simple script for weather sites scrapping
 """
 import sys
 import html
 import argparse
-from bs4 import BeautifulSoup
+import configparser
+from pathlib import Path
 from urllib.request import urlopen, Request
+
+from bs4 import BeautifulSoup
 
 ACCU_URL = "https://www.accuweather.com/uk/ua/kaniv/321864/weather-forecast/321864"
 ACCU_TAGS = ('<span class="large-temp">','<span class="cond">')
+ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
+
+
 
 RP5_URL = "http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9A%D0%B0%D0%BD%D0%B5%D0%B2%D1%96"
-#RP5_TAGS = (rp5_content.find(RP5_TEMP_TAG, rp5_content.find(WINFO_CONTAINER_TAG)), ',"°F</span>')
-#WINFO_CONTAINER_TAG = '<div id="ArchTemp">'
-#RP5_TEMP_TAG = '<span class="t_0" style="display: block;">'
 RP5_TAGS = ('<span class="t_0" style="display: block;">', '<span class="t_0" style="display: block;">')
 
 
@@ -21,14 +25,12 @@ SINOPTIK_URL = "https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%
 SINOPTIK_TAGS = ('<p class="today-temp">','<div class="description"> <!--noindex-->')
 
 
-
-
 def get_request_headers():
     return {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64;)'}
 
 def get_page_source(url):
-    """функція, де ми отримуємо url,
-       function where we get url
+    """функція, де ми отримуємо url і повертаємо html-код із сторінки,
+       a function where we get a url and return html-code from the page
     """
 
     request = Request(url, headers=get_request_headers())
@@ -53,9 +55,41 @@ def get_tag_content(page_content, tag):
     return content
 
 
+def get_locations(locations_url):
+    """
+    """
+    locations_page = get_page_source(locations_url)
+    soup = BeautifulSoup(locations_page, 'html.parser')
+    
+    locations = []
+    for location in soup.find_all('li', {'class': 'drilldown cl'}):
+        url = location.find('a').attrs['href']
+        location = location.find('em').text
+        locations.append((location, url)) 
+    return locations
+
+
+def configurate():
+    """виводить список локацій
+       displays a list of locations
+    """
+
+    locations = get_locations(ACCU_BROWSE_LOCATIONS)
+    while locations:
+        for index, location in enumerate(locations):
+            print(f'{index + 1}. {location[0]}')
+        selected_index = int(input('Please select location: '))
+        location = locations[selected_index - 1]
+        locations = get_locations(location[1])
+    
+    save_configuration(*location)
+
+
 def get_weather_info(page_content):
+    """get information about the weather conditions from the site
+       функція повертає інформацію про стан погоди 
     """
-    """
+
     city_page = BeautifulSoup(page_content, 'html.parser')
     
     current_day_section = city_page.find(
@@ -102,50 +136,57 @@ def get_weather_info(page_content):
 
 
 def produce_output(info):
-    """функція що виводить інформацю із сайтів
-       function that displays information from sites
+    """функція що виводить в консоль інформацю із сайта Accuweather
+       function that displays information from site Accuweather
     """
 
     print('Accu Weather: \n')
+    print(f'{city_name}')
+    print('+'*20)
 
     for key, value in info.items():
         print(f'{key}: {html.unescape(value)}')
 
-    """print(f'\n {provider_name}')
-    print(f'Temperature: {html.unescape(temp)}\n')
-    print(f'Condition: {html.unescape(condition)}\n')
+
+def get_accu_weather_info():
+    """функція, яка поверне інформацію про стан погоду за url-адресою, яку збережено у файлі концігурації
+       a function that returns the weather state information at the url-address stored in the concatenation file
     """
+
+    city_name, city_url = get_configuration()
+    content = get_page_source(city_url)
+    produce_output(city_name, get_weather_info(content))
+
 
 def main(argv):
     """ Main entry point.
     """
 
-    KNOWN_COMMANDS = {'accu': 'AccuWeather', 'rp5': 'RP5', 'sinoptik': 'SINOPTIK'}
+    # KNOWN_COMMANDS = {'accu': 'AccuWeather', 'rp5': 'RP5', 'sinoptik': 'SINOPTIK'}
+    KNOWN_COMMANDS = {'accu': get_accu_weather_info,
+                      'config': configurate}
 
     parser = argparse.ArgumentParser()
     parser.add_argument('command', help='Service name', nargs=1)
     params = parser.parse_args(argv)
 
-    weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS), 
+    '''weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS), 
                                     "RP5": (RP5_URL, RP5_TAGS), 
                                     "SINOPTIK": (SINOPTIK_URL, SINOPTIK_TAGS)} 
+                                    '''
     #, "PR5": (RP5_URL, RP5_TAGS)}
 
     if params.command:
         command = params.command[0]
         if command in KNOWN_COMMANDS:
-            weather_sites = {
-                KNOWN_COMMANDS[command]: weather_sites[KNOWN_COMMANDS[command]]
-            }
+            KNOWN_COMMANDS[command]()
         else:
             print("Unknown command provided!")
             sys.exit(1)
 
-    for name in weather_sites:
-        url, tags = weather_sites[name]
-        content = get_page_source(url)
-        produce_output(get_weather_info(content))
-
+    
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+
